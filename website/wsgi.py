381052d56cef6f2238c9c62e05e52dd911a8debf
Juan Menezes <juan.menezes@outlook.com>
2021-04-13 22:30:05 -0300

    mudei teste

--- website/wsgi.py
+++ website/wsgi.py
@@ -2,7 +2,7 @@
 WSGI config for website project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
+mudei isso aqui
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
