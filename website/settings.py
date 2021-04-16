808937f467bed32b115a5cb79f40ce9e2f58c71b
Mozart <mozartnp@gmail.com>
2021-04-16 11:35:39 -0300

    inclusão dos arquivos estaticos

--- website/settings.py
+++ website/settings.py
@@ -127,5 +127,6 @@ USE_TZ = True
 STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'website/static')
+    os.path.join(BASE_DIR, 'website/static/website'),
+    os.path.join(BASE_DIR, 'home/static/home'),
 ]
