0766d90102132648a574a473ccc1ae08dcb76e16
Mozart <mozartnp@gmail.com>
2021-04-23 23:40:30 -0300

    login e atutenticação

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method =='POST':
+        usuario = request.POST['usuario']
+        senha = request.POST['senha']
+
+        #Checagem de segurança para usuário e senha.
+        user = auth.authenticate(request, username=usuario, password=senha)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('manutencao')
+        else:
+            print('Usuário ou senha está errado.')
+            return render(request, 'login.html')
+
+    else:
+        return render(request, 'login.html')
 
 def manutencao(request):
-    return render(request, 'manutencao.html')
+
+    #Verificação de usuario para poder usar ou não a tela de manutencao
+    if request.user.is_authenticated:
+        return render(request, 'manutencao.html')
+    else:
+        return redirect('login')
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect('index')
