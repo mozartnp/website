6922d589644ea77b01f0ea3db3d53d5040fd65bf
Mozart <mozartnp@gmail.com>
2021-04-27 23:08:38 -0300

    migração para postgres

--- website/settings.py
+++ website/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'website',
-        'USER': 'root',
-        'PASSWORD': 'toor',
+        'USER': 'postgres',
+        'PASSWORD': '18054a807',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PORT': '5432',
     }
 }
 
