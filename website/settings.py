fd89748a14b194ae7b72117d80fb6446fc4265d8
MozartNP <mozartnp@gmail.com>
2021-12-13 20:07:00 -0300

    Atualização geral

--- website/settings.py
+++ website/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'website',
         'USER': 'postgres',
-        'PASSWORD': '18054a807',
+        'PASSWORD': '6046095',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
